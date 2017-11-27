f77ca0f7757fb893014c6761bb460d089bf1fa98
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2017-11-28 08:49:33 +1000

    Update

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import './App.css';
 import toDos from './data';
 import { Container, Row, Col, ListGroup, ListGroupItem, Badge } from 'reactstrap';
 
+function Heading(props) {
+  return (
+    <div class="heading">
+      <h1>
+        My Todo List
+      </h1>
+      <h2>
+        Things I need to do!
+      </h2>
+    </div>
+)};
+
 class App extends Component {
   render() {
     return (
-      <ul>
-        {toDos.map(item => (
-          <li>{ item }</li>
-        ))}
-      </ul>
+      <div>
+        <Heading />
+        <ListGroup>
+          {toDos.map(item => (
+            <ListGroupItem>{ item.task } - { item.status }</ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
     );
   }
 }
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 let toDos = [
-  'Wash Clothes',
-  'Finish Study',
-  'Feed Dog'
+  { id: 1, task:'Wash Clothes', status:'Incomplete' },
+  { id: 2, task:'Finish Study', status:'Complete' },
+  { id: 3, task:'Feed Dog', status:'Incomplete' }
 ]
 
 export default toDos;
